b0cfe63c49671c4a86b0579c2b964cf365890f43
Juan Felipe Torres <jf.torresp@outlook.com>
2025-01-31 09:50:47 -0500

    Initial Commit

---
+++ climaxApp/climaxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  climaxAppApp.swift
+//  climaxApp
+//
+//  Created by Juan Felipe Torres on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct climaxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
